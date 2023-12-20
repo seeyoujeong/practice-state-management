25cd822fbc403924922d1df83b1dd70b07a1a49f
seeyoujeong <chan3358@naver.com>
2023-12-20 10:27:45 +0900

    feat: 타입스크립트로 변경 및 초기화

--- src/constants/url.js
+++
@@ -1,4 +0,0 @@
-export const IMAGE_URL =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
-
-export const BASE_URL = "https://pokeapi.co/api/v2/";
