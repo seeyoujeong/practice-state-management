35c706a39cb8647baba6e503d3b0e22b4f35a0ea
seeyoujeong <chan3358@naver.com>
2024-04-26 23:53:42 +0900

    feat: 폼 추가

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,9 +1,31 @@
 import styled from "@emotion/styled";
+import { FormEventHandler, useState } from "react";
 
 export default function SearchPage() {
-  return <Container>search page</Container>;
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    console.log(inputValue);
+    setInputValue("");
+  };
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+        <button>검색</button>
+      </form>
+    </Container>
+  );
 }
 
 const Container = styled.div`
   height: calc(100% - 50px);
+  display: flex;
+  justify-content: center;
 `;
