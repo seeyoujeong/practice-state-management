0ed0f7454061bcc35668c5ca995c188388ac07ab
seeyoujeong <chan3358@naver.com>
2024-05-10 11:17:44 +0900

    refactor: 캐싱 추가

--- src/hooks/useKeywordQuerystring.ts
+++ src/hooks/useKeywordQuerystring.ts
@@ -1,13 +1,21 @@
+import { useCallback, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const useKeywordQuerystring = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const keywordQuerystring = searchParams.get("keyword") || "";
-  const setKeywordQuerystring = (keyword: string) => {
-    setSearchParams({ keyword });
-  };
 
-  return { keywordQuerystring, setKeywordQuerystring };
+  const setKeywordQuerystring = useCallback(
+    (keyword: string) => {
+      setSearchParams({ keyword });
+    },
+    [setSearchParams]
+  );
+
+  return useMemo(
+    () => ({ keywordQuerystring, setKeywordQuerystring }),
+    [keywordQuerystring, setKeywordQuerystring]
+  );
 };
 
 export default useKeywordQuerystring;
