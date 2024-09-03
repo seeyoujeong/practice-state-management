df6c031e765fc56bb068e0963b63413ac57b166e
seeyoujeong <chan3358@naver.com>
2024-09-03 14:32:07 +0900

    feat: CartContext 추가

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,60 @@
+import { createContext, useMemo, useReducer } from "react";
+
+interface CartItem {
+  name: string;
+}
+
+interface CartContextType {
+  cartItems: CartItem[];
+  dispatch: React.Dispatch<Action>;
+}
+
+export const CartContext = createContext<CartContextType>({
+  cartItems: [],
+  dispatch: () => {},
+});
+
+type Action =
+  | { type: "added"; name: string }
+  | { type: "deleted"; name: string }
+  | { type: "reset" };
+
+function cartReducer(state: CartItem[], action: Action) {
+  switch (action.type) {
+    case "added": {
+      const { name } = action;
+
+      if (state.some((prevState) => prevState.name === name)) {
+        return state;
+      }
+
+      return [{ name }, ...state];
+    }
+    case "deleted": {
+      const { name } = action;
+
+      return state.filter((prevState) => prevState.name !== name);
+    }
+    case "reset": {
+      return [];
+    }
+    default: {
+      throw new Error(`Unkown action: ${action}`);
+    }
+  }
+}
+
+export function CartProvider({ children }: React.PropsWithChildren) {
+  const [cartItems, dispatch] = useReducer(cartReducer, []);
+
+  const cartContextValue = useMemo(
+    () => ({ cartItems, dispatch }),
+    [cartItems]
+  );
+
+  return (
+    <CartContext.Provider value={cartContextValue}>
+      {children}
+    </CartContext.Provider>
+  );
+}
