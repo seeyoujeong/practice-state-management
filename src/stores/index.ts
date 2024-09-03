91819bc8f776136ee24168909e0bb5f75f98673b
seeyoujeong <chan3358@naver.com>
2024-09-03 16:54:12 +0900

    feat: useSearchStore 추가

---
+++ src/stores/index.ts
@@ -0,0 +1,1 @@
+export { default as useSearchStore } from "./useSearchStore";
---
+++ src/stores/useSearchStore.ts
@@ -0,0 +1,48 @@
+import { create } from "zustand";
+
+interface Searched {
+  keyword: string;
+  date: string;
+}
+
+type State = {
+  searchedList: Searched[];
+};
+
+type Actions = {
+  addKeyword: (keyword: string, date: string) => void;
+  deleteKeyword: (keyword: string) => void;
+  resetList: () => void;
+};
+
+const useSearchStore = create<State & Actions>((set) => ({
+  searchedList: [],
+  addKeyword: (keyword: string, date: string) =>
+    set((state) => {
+      if (
+        state.searchedList.find(
+          ({ keyword: prevKeyword }) => prevKeyword === keyword
+        )
+      ) {
+        const removedList = state.searchedList.filter(
+          ({ keyword: prevKeyword }) => prevKeyword !== keyword
+        );
+
+        return { ...state, searchedList: [{ keyword, date }, ...removedList] };
+      }
+
+      return {
+        ...state,
+        searchedList: [{ keyword, date }, ...state.searchedList],
+      };
+    }),
+  deleteKeyword: (keyword: string) =>
+    set((state) => ({
+      searchedList: state.searchedList.filter(
+        (prevState) => prevState.keyword !== keyword
+      ),
+    })),
+  resetList: () => set({ searchedList: [] }),
+}));
+
+export default useSearchStore;
